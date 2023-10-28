cff88464301c736a5dd74e96e32b27fa095bc317
duclh19 <lh.duc0112@gmail.com>
2023-10-29 01:56:30 +0700

    uncomment system prompt

--- src/backend/prompts/comparision_prompts.py
+++ src/backend/prompts/comparision_prompts.py
@@ -1,17 +1,10 @@
-# COMPARISION_SYSTEM_TEMPLATE = (
-#     "As a financial assistant, you will help users with finance-related problems based on the context provided by the user,"
-#     "which is a messages, and generating a specific and accurate response.\n"
-#     "If you are unable to provide a specific and accurate answer, you should indicate that you do not know.\n"
-#     "If the question is not related to the finance-related problems, you should indicate that you do not know.\n"
-#     "Always strive to be as helpful as possible to the user.\n"
-# )
-
-# COMPARISION_USER_TEMPLATE = (
-#     "Contexts:\n"
-#     "- Context 1: {context_1}\n"
-#     "- Context 2: {context_3}\n"
-#     "Helpful Answer: "
-# )
+COMPARISION_SYSTEM_TEMPLATE = (
+    "As a financial assistant, you will help users with finance-related problems based on the context provided by the user,"
+    "which is a messages, and generating a specific and accurate response.\n"
+    "If you are unable to provide a specific and accurate answer, you should indicate that you do not know.\n"
+    "If the question is not related to the finance-related problems, you should indicate that you do not know.\n"
+    "Always strive to be as helpful as possible to the user.\n"
+)
 
 template = """
 Your task is to help users to compare two different company given their business documents.\ 
