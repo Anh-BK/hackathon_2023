8e23a4ae784e696a13f61afa63d116e6f72773da
Đạt Huỳnh <datht22@fpt.com>
2023-10-28 10:53:31 +0700

    API company

--- src/app/api/v1/__init__.py
+++ src/app/api/v1/__init__.py
@@ -1,10 +1,8 @@
 from sys import prefix
 from fastapi import APIRouter
-from .answering import router as answering_router
-from .decision import router as decision_router
-from .user_profile import router as profile_router
+# from .answering import router as answering_router
+from .company import router as company_router
 
 api_v1_router = APIRouter()
-api_v1_router.include_router(answering_router, prefix="/answering",tags=["Answering"])
-api_v1_router.include_router(decision_router, prefix="/decision", tags=["Decision"])
-api_v1_router.include_router(profile_router, prefix="/profile",tags=["Profile"])
+# api_v1_router.include_router(answering_router, prefix="/answering",tags=["Answering"])
+api_v1_router.include_router(company_router, prefix="/company",tags=["Company"])
