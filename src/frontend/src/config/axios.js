ab5b87275b325b39e364043c0994f58b1b519ba4
Đạt Huỳnh <datht22@fpt.com>
2023-11-03 11:27:32 +0700

    refac: URL endpoint

--- src/frontend/src/config/axios.js
+++ src/frontend/src/config/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: "http://4.193.50.237:8002/api/v1",
+  baseURL: "http://localhost:8002/api/v1",
   headers: {
     "Content-Type": "application/json",
   },
